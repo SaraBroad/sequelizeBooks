05a9662f9aac381e99f66453002890e77397dc73
SaraBroad <saraebroad@gmail.com>
2020-08-21 20:43:40 -0400

    add folders

--- seeders/20200822002315-book-list.js
+++ seeders/20200822002315-book-list.js
@@ -1,24 +1,109 @@
-'use strict'
-
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+  up: (queryInterface, Sequelize) =>
+    queryInterface.bulkInsert('Books', [
+      {
+        id: 1,
+        title: 'Heavy',
+        author: 'Kiese Laymon',
+        dateFinished: '2019-01-21',
+        pages: 256,
+        rating: 5,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: 2,
+        title: 'Boy Swallows Universe',
+        author: 'Trent Dalton',
+        dateFinished: '2019-04-27',
+        pages: 464,
+        rating: 5,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: 3,
+        title: 'The Buried',
+        author: 'Peter Hessler',
+        dateFinished: '2019-05-22',
+        pages: 480,
+        rating: 5,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: 4,
+        title: 'The Body Papers',
+        author: 'Grace Talusan',
+        dateFinished: '2019-07-17',
+        pages: 256,
+        rating: 5,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: 5,
+        title: 'Daisy Jones & The Six',
+        author: 'Taylor Jenkins Reed',
+        dateFinished: '2019-11-06',
+        pages: 350,
+        rating: 4,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: 6,
+        title: 'Small Fry',
+        author: 'Lisa Jobs',
+        dateFinished: '2018-11-18',
+        pages: 383,
+        rating: 4,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: 7,
+        title: 'Boom Town',
+        author: 'Sam Anderson',
+        dateFinished: '2018-09-22',
+        pages: 448,
+        rating: 5,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: 8,
+        title: 'Flights',
+        author: 'Olga Tokarczuk',
+        dateFinished: '2018-10-18',
+        pages: 403,
+        rating: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: 9,
+        title: 'The Hearts Invisble Furies',
+        author: 'John Boyne',
+        dateFinished: '2019-08-18',
+        pages: 582,
+        rating: 5,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: 10,
+        title: 'The Great Believers',
+        author: 'Rebecca Mekkai',
+        dateFinished: '2019-01-01',
+        pages: 421,
+        rating: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]),
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Books', null, {})
   }
 }
--- seeders/20200822002336-add-book-type-data.js
+++ seeders/20200822002336-add-book-type-data.js
@@ -1,24 +1,58 @@
-'use strict'
+const { Book } = require('../models')
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  up: async function () {
+    return Promise.all(bookTypeAdditions.map(({ title, bookType }) =>
+      Book.update({ bookType }, { where: { title }, fields: ['bookType'] })
+    ))
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  down: async function () {
+    return Promise.all(bookTypeAdditions.map(({ title }) =>
+      Book.destory({ bookType: null }, { where: { title }, fields: ['bookType'] })
+    ))
   }
 }
+
+const bookTypeAdditions = [
+  {
+    title: 'Heavy',
+    bookType: 'nonfiction'
+  },
+  {
+    title: 'Boy Swallows Universe',
+    bookType: 'fiction'
+  },
+  {
+    title: 'The Buried',
+    bookType: 'nonfiction'
+  },
+  {
+    title: 'The Body Papers',
+    bookType: 'nonfiction'
+  },
+  {
+    title: 'Daisy Jones & The Six',
+    bookType: 'fiction'
+  },
+  {
+    title: 'Small Fry',
+    bookType: 'nonfiction'
+  },
+  {
+    title: 'Boom Town',
+    bookType: 'nonfiction'
+  },
+  {
+    title: 'Flights',
+    bookType: 'fiction'
+  },
+  {
+    title: 'The Hearts Invisble Furies',
+    bookType: 'fiction'
+  },
+  {
+    title: 'The Great Believers',
+    bookType: 'fiction'
+  }
+]
